0f5185bcd35c364b2b796f9e3a2ef72b1570df1c
danielalv07 <daniel090102@gmail.com>
2026-02-04 09:49:26 -0600

    Se agregó la Actividad 2 del Lab
    Se descargó el Visual Studio en las carpetas del laboratorio

---
+++ Laboratorio 3/Lab03-A02-Daniel Alvarado/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03_A02_Daniel_Alvarado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Programa hecho por Daniel Alvarado");
+        }
+    }
+}
