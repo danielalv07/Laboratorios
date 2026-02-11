ac064ecf57711e29db035aa274ede38052d1cfa2
danielalv07 <daniel090102@gmail.com>
2026-02-11 09:47:07 -0600

    Creación de Lab04 y hacer las primeras 4 actividades
    Se creo la carpeta del Lab 4 y se ingreso el proyecto de Visual Studio, se realizaron las primeras 4 actividades de las 7 totales

---
+++ Laboratorio 4/L4_DA_1248926/L4_DA_1248926/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4_DA_1248926
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio 1
+            string modelo = "FNasa12";
+            int carga = 3000;
+            float NivelCombustible = 25.5f;
+            Boolean SaltoActivo = true;
+            Console.WriteLine("Modelo: " + modelo);
+            Console.WriteLine("Carga: " + carga);
+            Console.WriteLine("Nivel del combustible " + NivelCombustible);
+            Console.WriteLine("¿Es de salto activo? " + SaltoActivo);
+
+            //Ejercicio 2
+            short sensoresActivos = 128;
+            int registroProcesador = Convert.ToInt32(sensoresActivos);
+            double precisionTotal = Convert.ToDouble(registroProcesador);
+            Console.WriteLine("Precisión total: " + precisionTotal);
+
+            //Ejercicio 3
+            double energiaGenerada = 987.65;
+
+            int energiaLimitada = (int)energiaGenerada;
+
+            Console.WriteLine("Energía Generada: " + energiaGenerada);
+            Console.WriteLine("Energía Limitada: " + energiaLimitada);
+
+            //Ejercicio 4
+            string entradaRadar;
+
+            Console.WriteLine("Ingrese la distancia en números del planeta más cercano");
+            entradaRadar = Console.ReadLine();
+
+            int distancia = int.Parse(entradaRadar);
+            distancia = distancia + 100;
+            Console.WriteLine("La distancia segura es: " + distancia);
+            
+
+
+
+
+
+
+        }
+    }
+}
