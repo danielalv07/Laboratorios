a701ecd8fd03b1c27c316c8fdf30a24d24d15c14
danielalv07 <daniel090102@gmail.com>
2026-02-10 21:06:16 -0600

    Proceso de VisualStudio Terminado

---
+++ Laboratorio 3/Lab02-A2-Daniel Alvarado/Lab02-A2-Daniel Alvarado/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02_A2_Daniel_Alvarado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Coloque su nombre");
+            string nombre;
+           nombre = Console.ReadLine();
+           Console.WriteLine("Coloque el nombre del curso");
+            string nombreCurso;
+           nombreCurso = Console.ReadLine();
+           Console.WriteLine("Lab de pensamiento Computacional");
+            Console.Write(nombre);
+            Console.Write(" vas muy bien en el curso de ");
+            Console.WriteLine(nombreCurso);
+            Console.WriteLine("Nunca te rindas.");
+           Console.WriteLine("Presione cualquier tecla para salir.");
+           Console.ReadKey();
+
+        }
+    }
+}
