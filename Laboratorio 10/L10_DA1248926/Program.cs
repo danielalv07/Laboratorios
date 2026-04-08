2a8994f5541351de8114d245e453fb81ebe8c481
Daniel Alvarado <daniel090102@gmail.com>
2026-04-08 09:14:15 -0600

    Creación del lab 10

---
+++ Laboratorio 10/L10_DA1248926/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L10_DA1248926
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
