8d14328a0d09abbd97c0d297bdfd15964123ed66
danielalv07 <daniel090102@gmail.com>
2026-04-19 21:08:55 -0600

    Edición sin terminar del Proyecto 11

--- Laboratorio 11/L11_DA_1248926/L11_DA_1248926/Program.cs
+++ Laboratorio 11/L11_DA_1248926/L11_DA_1248926/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,28 +15,58 @@ namespace L11_DA_1248926
             int largo;
             string contraseña;
             int mayus = 0;
+            int num = 0;
+            int numCorrecto = 0;
+            int num2 = 0;
             Console.WriteLine("Ingrese una contraseña con: ");
             Console.WriteLine("Al menos 8 caracteres");
             Console.WriteLine("Al menos 1 número");
             Console.WriteLine("Al menos un carácter especial");
             contraseña = Console.ReadLine();
             largo = contraseña.Length;
-
-            for (int i = 0; i < largo; i++)
+            if (largo < 8)
             {
-                if (largo < 8)
-                {
-                    Console.WriteLine("Contraseña inválida, no es lo suficientemente larga");
-                    
-                }
-                if (contraseña[i] == Char.ToUpper(contraseña[i]))
-                {
-                    mayus = 1;
-                    i = largo - 1;
-                }
-                if (mayus != 1) { Console.WriteLine("Contraseña inválida, no tiene mayúsculas"); }
+                Console.WriteLine("Contraseña inválida, no es lo suficientemente larga");
             }
+            else
+            {
+                for (int i = 0; i < largo; i++)
+                {
+                    if (contraseña[i] == Char.ToUpper(contraseña[i]))
+                    {
+                        mayus = 1;
+                    }
+                }
+                if (mayus != 1)
+                {
+                    Console.WriteLine("Contraseña inválida, no tiene mayúsculas");
+                }
+                else
+                {
+                    for (int j = 0; j < largo; j++)
+                    {
+                        for (int g = 0; g < 10; g++)
+                        {
+                            if (contraseña[j] == num)
+                            {
+                                numCorrecto = 1;
+                            }
+                            num = num + 1;
+                        }
 
+                    }
+                    if (numCorrecto != 1)
+                    {
+                        Console.WriteLine("Contraseña inválida, no tiene mnúmeros");
+                    }
+                    if (largo > 8 && mayus == 1 && numCorrecto == 1)
+                    {
+                        Console.WriteLine("Contraseña válida.");
+                    }
+                }
+            }   
         }
+
+
     }
 }
